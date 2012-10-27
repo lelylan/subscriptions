c98af617fdfdc6d4831a275bb6035a242816465a
Andrea Reginato <andrea.reginato@gmail.com>
2012-10-27 15:19:35 +0200

    Updated env variable for the subscription db. It was named webhooks

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Subscriptions::Application.config.session_store :cookie_store, key: '_lelylan_webhooks_session'
+Subscriptions::Application.config.session_store :cookie_store, key: '_lelylan_subscriptions_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
